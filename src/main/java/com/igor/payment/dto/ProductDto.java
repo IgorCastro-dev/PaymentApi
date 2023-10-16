6fe83fd87bc2ab94866de67d74f8cff8a1b3b29a
Igor Encarnação Junqueira Castro <igorjunqueira19@gmail.com>
2023-10-16 16:26:57 -0300

    feat: adicionando o ProductDto

---
+++ src/main/java/com/igor/payment/dto/ProductDto.java
@@ -0,0 +1,35 @@
+package com.igor.payment.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductDto {
+    private String id;
+
+    @NotBlank(message = "acronym é obrigatório")
+    @Size(min = 6,max = 20)
+    private String acronym;
+
+    @NotBlank(message = "name é obrigatório")
+    private String name;
+
+    @NotNull
+    @JsonProperty("current_price")
+    private BigDecimal currentPrice;
+
+    @JsonProperty(value = "dt_creation")
+    private LocalDate dtCreation;
+}
