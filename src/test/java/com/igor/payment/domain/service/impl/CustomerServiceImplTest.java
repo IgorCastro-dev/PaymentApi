39236778281cf405f19729ae954a4407f01681a4
Igor Encarnação Junqueira Castro <igorjunqueira19@gmail.com>
2023-11-05 18:22:00 -0300

    testando criar customer quando customer enão tá presente

--- src/test/java/com/igor/payment/domain/service/impl/CustomerServiceImplTest.java
+++ src/test/java/com/igor/payment/domain/service/impl/CustomerServiceImplTest.java
@@ -4,6 +4,7 @@ import com.igor.payment.domain.model.CustomerModel;
 import com.igor.payment.domain.repository.CustomerRepository;
 import com.igor.payment.dto.CustomerDto;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -21,12 +22,29 @@ class CustomerServiceImplTest {
     private CustomerServiceImpl customerService;
     @Mock
     private CustomerRepository customerRepository;
-    @Test
-    void give_create_when_customerByEmailIsPresent_then_returnCustomerModel() {
+    private CustomerDto customerDto;
+    private CustomerModel customerModel;
+
+    @BeforeEach
+    public void setup(){
         CustomerDto customerDto = new CustomerDto();
         customerDto.setEmail("t@test.com");
-        CustomerModel customerModel = new CustomerModel();
+        this.customerDto = customerDto;
+        this.customerModel = new CustomerModel();
+    }
+    @Test
+    void give_create_when_customerByEmailIsPresent_then_returnCustomerModel() {
         Mockito.when(customerRepository.findByEmail("t@test.com")).thenReturn(Optional.of(customerModel));
+        Mockito.when(customerRepository.save(customerModel)).thenReturn(customerModel);
+        Assertions.assertEquals(customerModel,customerService.create(customerDto));
+        Mockito.verify(customerRepository,Mockito.times(1)).findByEmail("t@test.com");
+        Mockito.verify(customerRepository,Mockito.times(1)).save(customerModel);
+    }
+
+    @Test
+    void give_create_when_customerByEmailIsNotPresent_then_returnCustomerModel() {
+        customerModel.setEmail("t@test.com");
+        Mockito.when(customerRepository.findByEmail("t@test.com")).thenReturn(Optional.empty());
         Mockito.when(customerRepository.save(customerModel)).thenReturn(customerModel);
         Assertions.assertEquals(customerModel,customerService.create(customerDto));
         Mockito.verify(customerRepository,Mockito.times(1)).findByEmail("t@test.com");
